caef77db061508904de1d67e3624f38ef2c4920c
EusaHack <eusa510@gmail.com>
2026-02-02 20:46:28 -0600

    feat : crear modelo contacto y configuracion de carpeta media

--- agenda_contactos/apps/agenda/models.py
+++ agenda_contactos/apps/agenda/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=60)
+    apellidos = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='fotos/', null=True, blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
